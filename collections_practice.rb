b8aad3e50c29bb292ef5f5e8bd562d0541b4a65b
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 22:55:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+array = []
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[0,1] = [1,0]
+end

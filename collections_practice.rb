871b5419b6d2a4c601872745cba00249d0a91571
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:09:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,55 +1,23 @@
-def sort_array_asc(arr)
-  arr.sort
-end
+array = []
 
-def sort_array_desc(arr)
-  arr.sort do |x,y|
-    y<=>x
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
   end
 end
 
-def sort_array_char_count
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
   array.sort do |a, b|
     a.length <=> b.length
   end
 end
 
-def swap_elements(arr)
-  arr[2], arr[1] = arr[1], arr[2]
-  arr
-end
-
-def swap_elements_from_to(arr,x,y)
-  arr[x], arr[y] = arr[y], arr[x]
-  arr
-end
-
-def reverse_array(arr)
-  arr.reverse
-end
-
-def kesha_maker(arr)
-  for i in 0...arr.length
-    arr[i][2] = "$"
-  end
-  arr
-end
-
-def find_a(arr)
-  arr.select do |x|
-    x[0] == "a"
-  end
-end
-
-def sum_array(arr)
-  arr.inject(:+)
-end
-
-def add_s(arr)
-  for i in 0...arr.length
-    if i != 1
-      arr[i] += "s"
-    end
-  end
-  arr
+def swap_elements(array)
+  foo.sort {|a,b| a <=> b}
 end
